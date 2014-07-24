3ad1fadf914837d41d959857caca5e966097153b
rishavs <rishav.sharan@live.com>
2014-07-24 13:31:51 +0530

    added artefacts layer

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var stage, renderer,
 
     gridLayer, tileLayer,
     
-    masterGameData = {"madpData" : {}}, // the object which contains things like definitions, mapdata, logic rules etc.
-    masterSceneData = {"tileProperties" : {}}, // the default sceneData that the serverSceneData is a copy of. it borrows a bit from masterGameData.
+    masterGameData = {"mapData" : {}}, // the object which contains things like definitions, mapdata, logic rules etc.
+    masterSceneData = {"tileDetails" : {}, "artefactDetails" :{}, "creatureDetails":{}}, // the default sceneData that the serverSceneData is a copy of. it borrows a bit from masterGameData.
     serverSceneData = {}, // the current scenData specific to a game instance
     clientSceneData = {}, // the client side subset of the serverSceneData specific to a game instance and a client
 
@@ -35,8 +35,8 @@ function init() {
     gridLayer = new PIXI.DisplayObjectContainer ();
     stage.addChild(gridLayer);
     
-    objLayer = new PIXI.DisplayObjectContainer ();
-    stage.addChild(objLayer);
+    artefactLayer = new PIXI.DisplayObjectContainer ();
+    stage.addChild(artefactLayer);
     
     uiLayer = new PIXI.DisplayObjectContainer ();
     stage.addChild(uiLayer);
@@ -60,8 +60,9 @@ function init() {
     
     // load other assets. may merge the jsonloader asset here
     var assetsList = [
-        '/images/sprites.json',
-        '/images/tiles2.json'
+        '/images/creatures.json',
+        '/images/tiles.json',
+        '/images/artefacts.json'
     ]
     var assetLoader = new PIXI.AssetLoader (assetsList)
     assetLoader.load();
@@ -71,9 +72,10 @@ function init() {
 
     assetLoader.on('onComplete', function(event) {
         textLog.setText('All Assets uploaded');
-        
+        sleep(1000);
         populateHexGrid();
         populateArtefacts();
+        populateObstacles();
         populateCreatures();
 
     });
@@ -82,15 +84,95 @@ function init() {
 };
 
 function populateArtefacts() {
-    textLog.setText('All Objects Populated');
-    console.log('All Objects Populated');
+    // we want to do two things. randomly populate crystals/artefacts in all the available positions and 
+    // ensuring that each crystal is represented in the map
+    var avalPosList = masterGameData.mapData.artefacts.crystals.positions;
+    var artefactObj = masterGameData.mapData.artefacts.crystals.types;
+    
+    var tempArtefactList = [];
+    for (var prop in artefactObj) {
+        if (artefactObj.hasOwnProperty(prop)) {
+            tempArtefactList.push(prop);
+        }
+    }
+
+    // to achieve this we will first populate every artefact from tempArtefactObj into the avalPosObj, making sure to trim the two arrays in each step. 
+    // then all the empty places in the avalPosObj will be randomly filled by artefactObj crystals.
+    while (avalPosList.length > 0) {      
+        var crystalType;
+        var crystalHexPos = avalPosList[ avalPosList.length * Math.random() << 0]
+        
+        // check if tempArtefactObj is empty.
+        // If not, use a random one from tempArtefactObj. trim the list then. if yes, use a random one from artefactObj
+        if (tempArtefactList.length === 0) {
+            crystalType = returnRandomObjectKey(artefactObj);
+        }
+        else {
+            crystalType = tempArtefactList[ tempArtefactList.length * Math.random() << 0]
+            removeFromArray (tempArtefactList, crystalType )
+        }
+        
+        createArtefact(crystalHexPos, crystalType);
+        
+        removeFromArray (avalPosList, crystalHexPos )
+    };
+
+    
+    textLog.setText('All Artefacts Populated');
+    console.log('All Artefacts Populated');
 }
-function createArtefact() {
-    textLog.setText('Artefact created');
-    console.log('Artefact created');
+
+function createArtefact(hexId, artefactName) {
+    var spriteCoords = getPosXnYFromGrid(hexId);
+    var spriteImg = masterGameData.mapData.artefacts.crystals.types[artefactName];
+    
+    var sprite = new PIXI.Sprite.fromImage(spriteImg);
+    // size = size-gap;
+    
+    sprite.x = spriteCoords[0];
+    sprite.y = spriteCoords[1];
+    
+    // set registration point to centre of the sprite
+    sprite.anchor.x = 0.5;
+    sprite.anchor.y = 0.5;
+    
+    artefactLayer.addChild(sprite);
+    
+    // create a concept of artefact id. add it to scenedata
+    sprite.gameObjectType = "artefact";
+    sprite.artefactId =  sprite.gameObjectType + "_" + artefactName + _.uniqueId('_');
+    
+    var tempObj = {};
+    //Leaving empty for now. May choose to add other attributes later or just remove this object.
+    masterSceneData.artefactDetails[sprite.artefactId] = tempObj;
+    
+    // set the "contains" value in scenedata hexdetails to the artefact Id.
+    masterSceneData.tileDetails[hexId].contains.artefact = [sprite.artefactId]
+}
+
+function populateObstacles() {
+    textLog.setText('All Obstacles Populated');
+    console.log('All Obstacles Populated');
+}
+
+function createObstacle() {
+    textLog.setText('Obstacle created');
+    console.log('Obstacle created');
 }
 
 function populateCreatures() {
+    
+    var creaturesObj = masterGameData.mapData.creatures;
+    
+    for (var prop in creaturesObj) {
+        if (!creaturesObj.hasOwnProperty(prop)) {
+            continue;
+        }
+        
+        console.log(prop);
+        
+    }
+    
     textLog.setText('All Creatures Populated');
     console.log('All Creatures Populated');
 }
@@ -158,11 +240,13 @@ function createHexTile(x,y, iterI, iterJ, name, type) {
     var cZ = iterI;
     var cY = -1*(cX+cZ);
     
+    sprite.gameObjectType = "hexTile";
     sprite.hexId = cX + "x" + cY + "y" + cZ + "z";
 
     sprite.interactive = true;
     sprite.click = function(mouseData){
         textLog.setText("MOUSE CLICK " + sprite.hexId);
+        console.log("MOUSE CLICK " + sprite.hexId);
     } 
 
     tileLayer.addChild(sprite);
@@ -170,9 +254,10 @@ function createHexTile(x,y, iterI, iterJ, name, type) {
     // since we dont want to read through the stage or display container every time we need some info on a tile, we will save it in a new array for easier access
     var tempObj = {};
     tempObj.posX = x;
-    tempObj.posy = y;
+    tempObj.posY = y;
+    tempObj.contains = {}; //setting occupied empty for now
 
-    masterSceneData.tileProperties[sprite.hexId] = tempObj;
+    masterSceneData.tileDetails[sprite.hexId] = tempObj;
 
 };
 
@@ -182,4 +267,47 @@ function animate() {
 
     // render the stage   
     renderer.render(stage);
+};
+
+function getPosXnYFromGrid (hexId) {
+    var posXnY = [masterSceneData.tileDetails[hexId].posX, masterSceneData.tileDetails[hexId].posY];
+    return posXnY;
+}
+
+function depthCompare(a,b) {
+  if (a.z < b.z)
+     return -1;
+  if (a.z > b.z)
+    return 1;
+  return 0;
+  // USAGE: myContainer.children.sort(depthCompare);
+  
+}
+
+// debug functions
+// ----------------------------------------------------------------------------------------------------------------------------
+function sleep( sleepDuration ){
+    console.log("Sleeping for", sleepDuration);
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+}
+
+//toolkit functions
+// ----------------------------------------------------------------------------------------------------------------------------
+function removeFromArray (array, element ) {
+    // I seriously have to write this? wow... how bass-ackwards is this lang?
+    while (array.indexOf(element) !== -1) {
+        array.splice(array.indexOf(element), 1);
+    }
+}
+
+function returnRandomObjectKey (obj) {
+    var keys = Object.keys(obj)
+    return keys[ keys.length * Math.random() << 0];
+};
+
+function returnRandomObjectProperty (obj) {
+    var keys = Object.keys(obj)
+    console.log(keys);
+    return obj[keys[ keys.length * Math.random() << 0]];
 };
